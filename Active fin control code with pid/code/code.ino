464de0f011729301d9bb60b529c22444df723968
sammy <sammyoina@gmail.com>
2022-11-22 23:27:32 +0300

    fix data loogging and add ota

--- Active fin control code with pid/code/code.ino
+++ Active fin control code with pid/code/code.ino
@@ -4,9 +4,19 @@
 #define ANGLE 90
 #include <SPI.h>
 #include <SD.h>
+#include <WiFi.h>
+#include <ESPmDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
 
 const int chipSelect = 5;
 File myFile;
+
+const char* ssid = "sammy2";
+const char* password = "12345678";
+
+SPIClass spiSD(HSPI);
 
 #define DATA_PIN    14
 
@@ -41,6 +51,16 @@ void MPUSetup();
 
 void setup() {
   Serial.begin(9600);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+  
   MPUSetup();
   MOTORSetup();
 
@@ -62,6 +82,38 @@ void setup() {
 
   // set master brightness control
   FastLED.setBrightness(BRIGHTNESS);
+
+  ArduinoOTA
+    .onStart([]() {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH)
+        type = "sketch";
+      else // U_SPIFFS
+        type = "filesystem";
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type);
+    })
+    .onEnd([]() {
+      Serial.println("\nEnd");
+    })
+    .onProgress([](unsigned int progress, unsigned int total) {
+      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    })
+    .onError([](ota_error_t error) {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+      else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    });
+
+  ArduinoOTA.begin();
+
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
@@ -93,7 +145,7 @@ void loop() {
   if (myFile) {
     myFile.print("error: ");
     myFile.print(error);
-    myFile.print" ,anglex: ");
+    myFile.print(" ,anglex: ");
     myFile.print(angleX);
     myFile.print(" ,angley: ");
     myFile.println(angleY);
@@ -127,4 +179,6 @@ void loop() {
 
 
    previousTime = currentTime;
+
+   ArduinoOTA.handle();
 }
