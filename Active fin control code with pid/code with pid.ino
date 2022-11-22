2c5456678d88485f037ccd279af1eabe8d9e04a5
Nataly Musilah <115026536+Musilah@users.noreply.github.com>
2022-11-22 14:00:27 +0300

    Add files via upload

---
+++ Active fin control code with pid/code with pid.ino
@@ -0,0 +1,83 @@
+#include <Wire.h>
+#include <ESP32Servo.h>
+#define ANGLE 90
+
+double x, y, z;
+int angleX, angleY;
+
+unsigned long currentTime, previousTime = 0;
+double elaspedTime, error, lastError = 0, cumError = 0, rateError, PID;
+const double setPoint = 0;
+float Kp = 100, Kd = 0, Ki = 0;
+
+int servopin_1 = 27;  //initializing the pins
+int servopin_2 = 26;
+int servopin_3 = 25;
+int servopin_4 = 32;
+
+Servo Servo1;  // creating servo objects
+Servo Servo2;
+Servo Servo3;
+Servo Servo4;
+
+
+void getAngle(double &A, double &B, double &C);
+void MPUSetup();
+
+void setup() {
+  Serial.begin(9600);
+  MPUSetup();
+  MOTORSetup();
+}
+
+void loop() {
+  currentTime = millis();
+  elaspedTime = currentTime - previousTime;
+  getAngle(x, y, z);  //update yaw, pitch and roll
+  error = setPoint - z;
+  rateError = (error - lastError) / elaspedTime;
+  cumError += error * elaspedTime;
+  PID = Kp * error + Kd * rateError + Ki * cumError;
+
+
+  angleX = ANGLE + PID;
+  angleY = ANGLE - PID;
+
+
+   angleX = constrain(angleX, 0, 180);
+   angleY = constrain(angleY, 0, 180);
+
+
+   MotorWrite(angleX, angleY);
+
+   Serial.print(x);
+   Serial.print(" ");
+   Serial.print(y);
+   Serial.print(" ");
+   Serial.print(z);
+   Serial.print(" Error = ");
+   Serial.print(error);
+   Serial.print(" proportional = ");
+   Serial.print( Kp * error);
+   Serial.print("  differential = ");
+   Serial.print(Kd * rateError);
+   Serial.print("  integral = ");
+   Serial.print(Ki * cumError);
+   Serial.print("  angleX = ");
+   Serial.print(angleX);
+   Serial.print("  angleY = ");
+   Serial.print(angleY);
+   Serial.print("  Error = ");
+   Serial.print(error);
+   Serial.print(" PID = ");  
+   Serial.println(PID);
+
+
+   previousTime = currentTime;
+}
+
+
+
+
+
+
