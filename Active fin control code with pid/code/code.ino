fe3bebd40f0c294e8db468913fbdbe6355152a2d
sammy <sammyoina@gmail.com>
2022-11-24 15:19:16 +0300

    flying code

--- Active fin control code with pid/code/code.ino
+++ Active fin control code with pid/code/code.ino
@@ -8,6 +8,7 @@
 #include <ESPmDNS.h>
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
+#include <EEPROM.h>
 
 
 const int chipSelect = 5;
@@ -28,12 +29,12 @@ SPIClass spiSD(HSPI);
 CRGB leds[NUM_LEDS];
 
 double x, y, z;
-int angleX, angleY;
+int angleX, angleY, prevangleX = 90, prevangleY = 90;
 
 unsigned long currentTime, previousTime = 0;
 double elaspedTime, error, lastError = 0, cumError = 0, rateError, PID;
 const double setPoint = 0;
-float Kp = 100, Kd = 0, Ki = 0;
+float Kp = 10, Kd = 1, Ki = 0;
 
 int servopin_1 = 27;  //initializing the pins
 int servopin_2 = 26;
@@ -48,9 +49,14 @@ Servo Servo4;
 
 void getAngle(double &A, double &B, double &C);
 void MPUSetup();
+void MOTORSetup();
+void MotorWrite(int a, int b);
 
 void setup() {
   Serial.begin(9600);
+
+  EEPROM.begin(1);
+  int resets = EEPROM.read(0);
 
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
@@ -60,7 +66,22 @@ void setup() {
     delay(5000);
     ESP.restart();
   }
-  
+
+
+  if (resets != 1) {
+    EEPROM.write(0,1);
+    EEPROM.commit();
+    delay(100);
+    resets = EEPROM.read(0);
+    Serial.print("resets: ");
+    Serial.println(resets);
+    delay(2000);
+    ESP.restart();
+  }
+
+  EEPROM.write(0,0);
+  EEPROM.commit();
+  delay(2000);
   MPUSetup();
   MOTORSetup();
 
@@ -71,7 +92,7 @@ void setup() {
 
   if (!SD.begin(chipSelect, spiSD)) {
     Serial.println("initialization failed!");
-    while (1);
+    //while (1);
   }
   Serial.println("initialization done.");
 
@@ -120,14 +141,20 @@ void loop() {
   currentTime = millis();
   elaspedTime = currentTime - previousTime;
   getAngle(x, y, z);  //update yaw, pitch and roll
-  if (z < 0){
+  if (y < 0){
     leds[0] = CRGB::Red;
     FastLED.show();
   }else{
     leds[0] = CRGB::Green;
     FastLED.show();
   }
-  error = setPoint - z;
+  error = setPoint - y;
+  if(abs(error - lastError) < 5){          
+MotorWrite(0, 0);
+  }
+  else {
+  
+  }
   rateError = (error - lastError) / elaspedTime;
   cumError += error * elaspedTime;
   PID = Kp * error + Kd * rateError + Ki * cumError;
@@ -137,10 +164,10 @@ void loop() {
   angleY = ANGLE - PID;
 
 
-   angleX = constrain(angleX, 0, 180);
-   angleY = constrain(angleY, 0, 180);
+   angleX = constrain(angleX, 0, 30);
+   angleY = constrain(angleY, 0, 30);
 
-   myFile = SD.open("/test.txt", FILE_WRITE);
+   /*myFile = SD.open("/test.txt", FILE_APPEND);
 
   if (myFile) {
     myFile.print("error: ");
@@ -150,9 +177,11 @@ void loop() {
     myFile.print(" ,angley: ");
     myFile.println(angleY);
   }
-  myFile.close();
+  myFile.close();*/
 
-
+    prevangleX =  angleX;
+  prevangleY = angleY;   
+  
    MotorWrite(angleX, angleY);
 
    Serial.print(x);
@@ -178,7 +207,63 @@ void loop() {
    Serial.println(PID);
 
 
+  lastError = error;
    previousTime = currentTime;
 
    ArduinoOTA.handle();
+}
+
+void MOTORSetup() {
+  Servo1.setPeriodHertz(50);
+  Servo2.setPeriodHertz(50);
+  Servo3.setPeriodHertz(50);
+  Servo4.setPeriodHertz(50);
+
+  Servo1.attach(servopin_1, 500, 2400);
+  Servo2.attach(servopin_2, 500, 2400);
+  Servo3.attach(servopin_3, 500, 2400);
+  Servo4.attach(servopin_4, 500, 2400);
+}
+
+void MotorWrite(int a, int b){
+  Servo1.write(a);
+  Servo2.write(a);
+  Servo3.write(b);
+  Servo4.write(b);  
+}
+
+void MPUSetup() {
+  Wire.setClock(400000);
+  Wire.begin();
+  delay(10);
+  Wire.beginTransmission(0x69);
+  Wire.write(0x6B);
+  Wire.write(0x00);
+  Wire.endTransmission(true);
+}
+
+void getAngle(double &A, double &B, double &C) {
+  int16_t Gyro_X, Gyro_Y, Gyro_Z;// Ac_X, Ac_Y, Ac_Z;
+  // int minV = 265;
+  // int maxV = 402;
+  Wire.beginTransmission(0x69);
+  Wire.write(0x43);
+  Wire.endTransmission(false);
+  Wire.requestFrom(0x69, 6);
+
+  Gyro_X = Wire.read() << 8 | Wire.read();
+  Gyro_Y = Wire.read() << 8 | Wire.read();
+  Gyro_Z = Wire.read() << 8 | Wire.read();
+
+ 
+
+  // A = RAD_TO_DEG * (atan2(-Gyro_Y, -Gyro_Z) + PI);
+  // B = RAD_TO_DEG * (atan2(-Gyro_X, -Gyro_Z) + PI);
+  // C = RAD_TO_DEG * (atan2(-Gyro_Y, -Gyro_X) + PI);
+A = Gyro_X / 131.0;
+B = Gyro_Y / 131.0;
+C = Gyro_Z / 131.0;
+
+
+  
 }
